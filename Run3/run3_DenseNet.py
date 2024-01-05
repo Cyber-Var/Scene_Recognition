8c304f54fc6bce8c985fb1972d432b60d41a2650
Code Padawan <92408725+LucianSerbanescu@users.noreply.github.com>
2024-01-05 23:09:38 +0200

    back_up Alexnet and densenet completed

--- Run3/run3_DenseNet.py
+++ Run3/run3_DenseNet.py
@@ -3,12 +3,10 @@ from sklearn.metrics import average_precision_score
 from sklearn.preprocessing import LabelEncoder
 
 import tensorflow as tf
-from tensorflow.keras import models, layers, optimizers, regularizers
-from tensorflow.keras.applications.densenet import preprocess_input
 from tensorflow.keras import layers, models
-
 from keras.src.applications import DenseNet201
-
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.keras.applications.densenet import preprocess_input
 
 from functions.run3_functions import *
 
@@ -20,13 +18,14 @@ dataset_path = os.path.join(script_dir, "..", "training")
 image_paths, labels = load_dataset(dataset_path)
 
 # Example usage:
-average_height, average_weight = calculate_images_int_average_size(image_paths)
+average_height, average_width = calculate_images_int_average_size(image_paths)
 
 print("Average Height:", average_height)
-print("Average Weight:", average_weight)
+print("Average Weight:", average_width)
 
 # Preprocess images and create feature vectors
-processed_images_input_layer = [process_image(img_path, average_height, average_weight) for img_path in image_paths]
+processed_images_input_layer = [process_image_densenet_3d(img_path, average_height, average_width) for img_path in
+                                image_paths]
 
 # Print the shapes of the preprocessed images
 for i, img in enumerate(processed_images_input_layer):
@@ -42,25 +41,38 @@ y = np.array(y, dtype=np.int32)
 
 # MODEL TRAINING
 
-# Check if GPU is available
-if tf.test.gpu_device_name():
-    print('GPU available:', tf.test.gpu_device_name())
-    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-else:
-    print("No GPU detected. Switching to CPU.")
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=47, stratify=y)
 
+# # Hyperparameters based on paper
+# epochs = 10
+# batch_size = 32
+# learning_rate = 0.001
+# last_layer_learning_rate = 0.01
+# weight_decay = 0.0005
+# momentum = 0.9
+
 # Hyperparameters based on paper
-epochs = 10
+epochs = 15
 batch_size = 32
-learning_rate = 0.001
-last_layer_learning_rate = 0.01
-weight_decay = 0.0005
-momentum = 0.9
+learning_rate = 0.0001
+last_layer_learning_rate = 0.001
+weight_decay = 1e-5
+# weight_decay = 0.0005
+
+# Checkpoint path
+checkpoint_path = 'densenet_best_model.h5'
+
+# ModelCheckpoint callback
+model_checkpoint = ModelCheckpoint(
+    filepath=checkpoint_path,
+    save_best_only=True,
+    monitor='val_loss',  # Monitor the validation loss
+    mode='min',  # Save when the monitored quantity is minimized
+    verbose=2  # Display informative messages
+)
 
 
-def create_densenet(input_shape=(average_height, average_weight, 3), num_classes=15):
+def dense_net_201(input_shape=(average_height, average_width, 3), num_classes=15):
     base_model = DenseNet201(weights='imagenet', include_top=False, input_shape=input_shape)
     base_model.trainable = False
 
@@ -81,31 +93,39 @@ def create_densenet(input_shape=(average_height, average_weight, 3), num_classes
     return model
 
 
+# Check if GPU is available
+if tf.test.gpu_device_name():
+    print('GPU available:', tf.test.gpu_device_name())
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+else:
+    print("No GPU detected. Switching to CPU.")
+
 # Train the model on GPU
 with tf.device('/GPU:0'):
     # Build the model
-    densenet_model = create_densenet()
+    densenet_model = dense_net_201()
 
     # Display the model summary
     densenet_model.summary()
 
-    # preprocess_input() is adjusting the data with each batch in each iteration
-    history = densenet_model.fit(preprocess_input(X_train), y_train, epochs=epochs, batch_size=batch_size,
-                                 validation_data=(preprocess_input(X_test), y_test))
+    # Train the model on GPU
+    fitted_model = densenet_model.fit(preprocess_input(X_train), y_train, epochs=epochs, batch_size=batch_size,
+                                      validation_data=(preprocess_input(X_test), y_test),
+                                      callbacks=[model_checkpoint])
 
+# Load the best model
+best_model = models.load_model(checkpoint_path)
 
 # MODEL PREDICTIONS
 
 # Predict probabilities on the test set using the features
 y_predicted = densenet_model.predict(preprocess_input(X_test))
 
-
 # MODEL EVALUATION
 
 # Evaluate the model on the test data and print accuracy
-test_loss, test_accuracy = densenet_model.evaluate(preprocess_input(X_test), y_test)
+test_loss, test_accuracy = best_model.evaluate(preprocess_input(X_test), y_test)
 print(f'\nTest Accuracy: {test_accuracy * 100:.2f}%')
-
 
 # Calculate Average Precision for each class
 average_precisions = []
@@ -122,4 +142,3 @@ for class_index in range(15):
 # Calculate and print the mean Average Precision (mAP)
 mean_average_precision = np.mean(average_precisions)
 print(f'Mean Average Precision (mAP): {mean_average_precision:.4f}')
-
--- functions/run3_functions.py
+++ functions/run3_functions.py
@@ -29,39 +29,54 @@ def get_image_size(image_path):
 
 
 def calculate_images_int_average_size(image_paths):
-    # Initialize variables to accumulate total height and weight
+    # Initialize variables to accumulate total height and width
     total_height = 0
-    total_weight = 0
+    total_width = 0
 
     # Iterate through each image path
     for image_path in image_paths:
         # Get the size of the current image
         image_size = get_image_size(image_path)
 
-        # Assuming your image size consists of (height, weight)
-        height, weight = image_size
+        # Assuming your image size consists of (height, width)
+        height, width = image_size
 
-        # Accumulate the height and weight
+        # Accumulate the height and width
         total_height += height
-        total_weight += weight
+        total_width += width
 
-    # Calculate the average height and weight
+    # Calculate the average height and width
     average_height = total_height / len(image_paths)
-    average_weight = total_weight / len(image_paths)
+    average_width = total_width / len(image_paths)
 
     # Return the result
-    return int(round(average_height)), int(round(average_weight))
+    return int(round(average_height)), int(round(average_width))
 
 
-def resize_image(image, average_height, average_weight):
-    # Resize the image, resize() takes (weight, height) as input
-    resized_img = image.resize((average_weight, average_height))
+def resize_image(image, average_height, average_width):
+    # Resize the image, resize() takes (width, height) as input
+    resized_img = image.resize((average_width, average_height))
     return resized_img
 
 
-def process_image(image_path, average_height, average_weight):
+def process_image_densenet_3d(image_path, average_height, average_width):
     with Image.open(image_path) as img:
-        resized_image = resize_image(img, average_height, average_weight)
+        resized_image = resize_image(img, average_height, average_width)
         image_np_array = np.array(resized_image)
         # convert the image to 3 channels, so it can be processed by the model
         return np.stack((image_np_array,) * 3, axis=-1)  # Convert single-channel to three identical channels
+
+
+def zero_mean_normalize(image):
+    mean_value = np.mean(image)
+    std_value = np.std(image)
+    normalized_image = (image - mean_value) /std_value
+    return normalized_image
+
+
+def process_image_alexnet(image_path, average_height, average_width):
+    with Image.open(image_path) as img:
+        resized_image = resize_image(img, average_height, average_width)
+        image_np_array = np.array(resized_image)
+        nor_img = zero_mean_normalize(image_np_array)
+        return nor_img
